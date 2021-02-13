aaef414be30a6fc6f726cee9d44241ce9e218d8a
Daniel Intrup <d.intrup@googlemail.com>
2021-02-13 23:08:33 +0100

    Startet jetzt automatisch

--- heiseminus.js
+++ heiseminus.js
@@ -1,10 +1,11 @@
 console.log("Let it GO");
+
 heiseminus = document.getElementsByClassName("heiseplus-logo-small")
+
 for (let index = 0; index < heiseminus.length; index++) {
     const artikel = heiseminus[index];
     const parent = findParentTag(artikel, "ARTICLE");
     parent.style.display = "none";
-    //artikel.parentElement.parentElement.parentElement.parentElement.style.display = "none"
 }
 
 function findParentTag(el, tag) {
