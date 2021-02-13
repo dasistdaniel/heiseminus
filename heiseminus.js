03f6e566062d59c721515509897809c3314b7bc8
Daniel Intrup <d.intrup@googlemail.com>
2021-02-13 22:54:26 +0100

    Sollte etwas robuster sein und besser als Vater von Vater von Vater...

--- heiseminus.js
+++ heiseminus.js
@@ -2,5 +2,16 @@ console.log("Let it GO");
 heiseminus = document.getElementsByClassName("heiseplus-logo-small")
 for (let index = 0; index < heiseminus.length; index++) {
     const artikel = heiseminus[index];
-    artikel.parentElement.parentElement.parentElement.parentElement.style.display = "none"
+    const parent = findParentTag(artikel, "ARTICLE");
+    parent.style.display = "none";
+    //artikel.parentElement.parentElement.parentElement.parentElement.style.display = "none"
+}
+
+function findParentTag(el, tag) {
+    while (el.parentNode) {
+        el = el.parentNode;
+        if (el.tagName === tag)
+            return el;
+    }
+    return null;
 }
